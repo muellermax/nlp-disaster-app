19e745b6abb9c0968d236a086a02f5cbc2f3ec44
unknown <muellermax1985@gmail.com>
2021-01-27 14:09:51 +0100

    Working on Deployment

---
+++ utils.py
@@ -0,0 +1,28 @@
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from nltk.corpus import stopwords
+
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+import pickle
+
+def tokenize(text):
+    """
+    Function to tokenize and lemmatize a given text.
+    :param text: String that has to be tokenized and lemmatized.
+    :return: List of tokenized words.
+    """
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        if tok not in stopwords.words('english'):
+            clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+            clean_tokens.append(clean_tok)
+
+    return clean_tokens
